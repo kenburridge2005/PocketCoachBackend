fdedc63e462bbb7d8f34e17c27957bfc27f4d842
kenburridge2005 <Kenneth.burridge2005@gmail.com>
2025-05-07 02:40:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def mealplan():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
